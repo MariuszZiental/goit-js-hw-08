6483244246c130bf7bbc7135c48c04033a6f1d35
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-08-14 18:56:02 +0200

    hw8

--- js/02-video.js
+++ js/02-video.js
@@ -1,2 +1,16 @@
-import VimeoPlayer from '@vimeo/player';
-import throttle from 'lodash.trottle';
+'use strict';
+
+import Player from "@vimeo/player";
+import VimeoPlayer from "@vimeo/player";
+import throttle from "lodash.trottle";
+
+const player = new Player('vimeo-player', {
+id: 236203659,
+})
+const throttleRun = throttle(onPlay, 1000);
+
+player.on('timeupdate', throttleRun);
+
+function onPlay(data) {
+    localStorage.setItem('videoPlayer')
+}
