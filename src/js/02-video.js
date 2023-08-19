28834d28ec35401e84eb91f1fc6d3e0952a0e5ae
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-08-19 18:27:22 +0200

    hw8 podejscie 4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,2 @@
 'use strict';
 import Player from '@vimeo/player';
-
-const player = new Player('handstick', {
-  id: 19231868,
-  width: 640,
-});
-
-player.on('play', function () {
-  console.log('played the video!');
-});
