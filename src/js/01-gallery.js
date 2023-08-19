938467a1b2c8ce2ba3c6e87c2f11e4f43dd550ac
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-08-19 17:25:32 +0200

    hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,9 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
