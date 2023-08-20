9845aa1a6ebbb9c0a26eedafcfda824708e6905d
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-08-20 19:36:12 +0200

    hw8 podejscie 7

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -27,3 +27,30 @@ function clearMessage(e) {
   console.log(`message: ${add.message}`);
   localStorage.clear();
 }
+
+function saveMessage(e) {
+  e.preventDefault();
+  const add = {
+    email: input.value,
+    message: message.value,
+  };
+  if (e.target === input) {
+    add.email = e.target.value;
+  } else if (e.target === message) {
+    add.message = e.target.value;
+  }
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(add));
+}
+
+function updateOutput() {
+  try {
+    const dataFromLS = localStorage.getItem(LOCALSTORAGE_KEY);
+    const dataFromLSParsed = JSON.parse(dataFromLS);
+    if (dataFromLS) {
+      input.value = dataFromLSParsed.email || '';
+      message.value = dataFromLSParsed.message || '';
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+}
