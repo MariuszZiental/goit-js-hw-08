b683814b5c72c92e4e87be24a08950acac17a1d9
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-09-01 00:18:55 +0200

    hw8 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,26 +3,27 @@ import throttle from 'lodash.throttle';
 const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
 const form = document.querySelector('.feedback-form');
-const { email, message } = form.elements;
 
 const LOCAL_KEY = 'feedback-form-state';
 
-if (localStorage.length !== 0) {
-  input.value = JSON.parse(localStorage.getItem(LOCAL_KEY)).email;
-  textarea.value = JSON.parse(localStorage.getItem(LOCAL_KEY)).message;
+const lsData = localStorage.getItem(LOCAL_KEY);
+if (lsData) {
+  const parsedData = JSON.parse(lsData);
+  input.value = parsedData.email;
+  textarea.value = parsedData.message;
 }
 
 form.addEventListener('input', throttle(getInputData, 500));
 form.addEventListener('submit', submit);
 
 function getInputData(event) {
-  const inputData = { email: email.value, message: message.value };
+  const inputData = { input: input.value, message: textarea.value };
   localStorage.setItem(LOCAL_KEY, JSON.stringify(inputData));
 }
 
 function submit(event) {
   event.preventDefault();
-  console.log({ email: email.value, message: message.value });
+  console.log({ input: input.value, textarea: textarea.value });
   form.reset();
   localStorage.clear();
 }
